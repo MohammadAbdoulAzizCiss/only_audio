c592f55b24b9537903a50cea921e635ef36d6b18
MohammadAbdoulAzizCiss <azizciss6@gmail.com>
2022-02-03 17:38:49 +0000

    smoking the zaza

--- screens/Otp.js
+++ screens/Otp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Container from '../components/Container';
+import Logo from '../assets/audio.svg';
 
 const OTPScreen = () => {
   return (
     <Container>
       <Text>OTP</Text>
+      <Logo />
     </Container>
   );
 };
