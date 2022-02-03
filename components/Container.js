4786703aa6d4cec6e3e58b534ac27a3d35de81e2
MohammadAbdoulAzizCiss <azizciss6@gmail.com>
2022-02-03 17:27:56 +0000

    ...

--- components/Container.js
+++ components/Container.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
-import { LESS_BLUE } from '../colors';
+import {View} from 'react-native';
+import {LESS_BLUE} from '../colors';
 
-export default function Container() {
-  return <View style={{backgroundColor: LESS_BLUE, flex: 1}}>
-
-
-  </View>;
+export default function Container({children}) {
+  return <View style={{backgroundColor: LESS_BLUE, flex: 1}}>{children}</View>;
 }
---
+++ screens/Otp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Container from '../components/Container';
+
+const OTPScreen = () => {
+  return (
+    <Container>
+      <Text>OTP</Text>
+    </Container>
+  );
+};
+
+export default OTPScreen;
